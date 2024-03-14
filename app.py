dfb7a38f518fd0654f1a7a7d8273c0bb15ca0fe5
CarlosVF2003 <162856389+CarlosVF2003@users.noreply.github.com>
2024-03-14 18:13:08 +0000

    commit

--- app.py
+++ app.py
@@ -26,23 +26,24 @@ print(Progreso_ind)
 #Registro de datos.
 # Crear un botón para registrar los datos
 with st.form(key='mi_formulario'):
-    Dia = st.text_input('Ingresa el Dia:')
-    Persona = st.selectbox('Su nombre:', ('Carlos', 'Cinthia'))
-    Maquina = st.selectbox('Selecciona una maquina:', ('Prensa de Piernas', 'Multipowers', 'Máquina de Extensión de Cuádriceps', 'Máquina de Femorales', 'Máquina de Aductores', 'Máquina de Abductores'))
-    Peso = st.slider('Selecciona el peso:', 0, 100, 40)
-    Descanso = st.selectbox('Selecciona la cantidad de tiempo:', ('1-2 min', '2-3 min', '3-4 min'))
-    Series = st.slider('Selecciona la cantidad de series:', 0, 4, 3)
-    Repeticiones = st.slider('Selecciona las repeticiones:', 0, 30, 15)
-    
+    # Tus widgets de entrada aquí
+    # ...
+
     # Botón de envío del formulario
     submit_button = st.form_submit_button(label='Guardar')
 
 # Procesar la información una vez que se envía el formulario
 if submit_button:
+    # Suponiendo que ya has recogido todos los datos en las variables correspondientes
     Progreso_new = {'Dia': Dia, 'Persona': Persona, 'Maquina': Maquina, 'Peso': Peso, 'Descanso': Descanso, 'Series': Series, 'Repeticiones': Repeticiones}
-    # Asegúrate de que Progreso_ind ya esté definido antes de este punto, posiblemente como un DataFrame vacío
+    
+    # Añadir los nuevos datos al DataFrame existente
     Progreso_ind = pd.concat([Progreso, pd.DataFrame([Progreso_new])], ignore_index=True)
-    Progreso_ind.to_csv('Libro.csv')
+    
+    # Guardar el DataFrame actualizado en un archivo CSV
+    Progreso_ind.to_csv('Libro.csv', index=False)
+    
+    # Mensaje de éxito
     st.success('¡Datos registrados con éxito!')
 
 # Mostrar el DataFrame resultante
