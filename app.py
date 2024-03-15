6781e0aaaddd9b5da2065ef405faf87361736dd1
CarlosVF2003 <162856389+CarlosVF2003@users.noreply.github.com>
2024-03-15 22:22:11 +0000

    commit

--- app.py
+++ app.py
@@ -79,24 +79,20 @@ with pestaña1:
                     # Guardar el DataFrame actualizado en un archivo CSV
                     # Utiliza transform para agregar la columna de conteo directamente al DataFrame existente
                     st.session_state['Progreso_ind']['Sets'] = st.session_state['Progreso_ind'].groupby(['Dia', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])['Peso'].transform('size')
-                    
-                    # Mensaje de éxito
+                    st.session_state['Progreso_ind']
                     st.success('¡Datos registrados con éxito!')
                     
                     # Ocultar el formulario
                     st.session_state['show_enfoque_form'] = False
                 else:
                     st.warning('Por favor completa todos los campos del formulario.')
-                
-        save_button = st.button(laben= 'Save')
-        if save_button:
-            st.session_state['Progreso_ind'].to_csv('Libro1.csv', index=False, sep=';')
+            
 
     # Visualización de datos
     st.subheader("Visualización de datos registrados")
     # Eliminar filas duplicadas basadas en las columnas específicas y actualizar los sets
     unique_values = st.session_state['Progreso_ind'].drop_duplicates(subset=['Dia', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])
-    st.write(unique_values)
+    st.write(st.session_state['Progreso_ind'])
     # Gráfico de comparación entre personas
     st.subheader("Comparación de progreso entre personas")
     avg_peso = st.session_state['Progreso_ind'].groupby('Persona')['Peso'].mean().reset_index()
