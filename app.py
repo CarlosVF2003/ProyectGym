2d723f60a81f78ee13e6aed07e1eb521e809d6e7
Carlos Alexander Vera Figueroa <162856389+CarlosVF2003@users.noreply.github.com>
2024-03-18 09:36:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def formulario_hipertrofia_muscular(sets):
     descanso = st.selectbox('Tiempo de descanso:', ('1-2 min', '2-3 min', '3-4 min'))
     return [peso] * sets, [repeticiones] * sets, [descanso] * sets  # Tanto el peso, las repeticiones y el tiempo de descanso son constantes para la hipertrofia muscular
 
-st.title('🏋️‍♂️ Reporte de Progreso en el Gimnasio 🏋️‍♀️')
+st.title('🏋️‍♂️ Nuestro progreso en el Gimnasio 🏋️‍♀️')
 
 # Botón para abrir el formulario principal
 if st.button("📝 Abrir Formulario Principal"):
@@ -52,11 +52,11 @@ if st.button("📝 Abrir Formulario Principal"):
 if st.session_state.get('show_enfoque_form', False):
     with st.form(key='mi_formulario'):
         # Widgets de entrada
-        Dia = st.text_input('📅 Ingresa el Día:')
-        Persona = st.selectbox('👤 Selecciona tu nombre:', ('Carlos', 'Cinthia'))
-        Maquina = st.selectbox('💪 Selecciona una máquina:', ('Prensa de Piernas', 'Multipowers', 'Máquina de Extensión de Cuádriceps', 'Máquina de Femorales', 'Máquina de Aductores', 'Máquina de Abductores','Press de pecho','Extension de hombro',
+        Dia = st.text_input('Ingresa el Día 📆:')
+        Persona = st.selectbox('Selecciona tu nombre 🤵‍♂️🙍:', ('Carlos', 'Cinthia'))
+        Maquina = st.selectbox('Selecciona una máquina 🏋️‍♀️🏋️‍♂️:', ('Prensa de Piernas', 'Multipowers', 'Máquina de Extensión de Cuádriceps', 'Máquina de Femorales', 'Máquina de Aductores', 'Máquina de Abductores','Press de pecho','Extension de hombro',
                                                                     'Extension tricep en polea','Extension lateral','Extension frontal'))
-        Enfoque = st.selectbox('🎯 Selecciona el enfoque de entrenamiento:', ('Desarrollo de Fuerza', 'Mejora de la Resistencia', 'Hipertrofia Muscular'))
+        Enfoque = st.selectbox('Selecciona el enfoque de entrenamiento:', ('Desarrollo de Fuerza', 'Mejora de la Resistencia', 'Hipertrofia Muscular'))
         sets = st.number_input('Número de sets:', min_value=1, max_value=10, step=1, value=4)
             
         # Botón de envío del formulario
@@ -74,11 +74,11 @@ if st.session_state.get('show_enfoque_form', False):
                 
             if form_completo:
                 for peso, repeticion, descanso in zip(pesos, repeticiones, descansos):
-                    Progreso_new = {'Día': Dia, 'Persona': Persona, 'Maquina': Maquina, 'Peso': peso, 'Descanso': descanso, 'Sets': sets, 'Repeticiones': repeticion}
+                    Progreso_new = {'Dia': Dia, 'Persona': Persona, 'Maquina': Maquina, 'Peso': peso, 'Descanso': descanso, 'Sets': sets, 'Repeticiones': repeticion}
                     st.session_state['Progreso_ind'] = pd.concat([st.session_state['Progreso_ind'], pd.DataFrame([Progreso_new])], ignore_index=True)
                 # Guardar el DataFrame actualizado en un archivo CSV
                 # Utiliza transform para agregar la columna de conteo directamente al DataFrame existente
-                st.session_state['Progreso_ind']['Sets'] = st.session_state['Progreso_ind'].groupby(['Día', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])['Peso'].transform('size')
+                st.session_state['Progreso_ind']['Sets'] = st.session_state['Progreso_ind'].groupby(['Dia', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])['Peso'].transform('size')
                 st.session_state['show_enfoque_form'] = False
                 st.success('¡Datos registrados con éxito!')
                 st.session_state['Progreso_ind'].to_csv('Progreso.csv', index= False, sep= ';')
@@ -87,33 +87,38 @@ if st.session_state.get('show_enfoque_form', False):
                 st.warning('Por favor completa todos los campos del formulario.')
 
 # Visualización de datos
-st.subheader("📊 Visualización de datos registrados")
+st.subheader("Visualización de datos registrados")
 # Eliminar filas duplicadas basadas en las columnas específicas y actualizar los sets
-unique_values = st.session_state['Progreso_ind'].drop_duplicates(subset=['Día', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])
-st.write(unique_values[['Día', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Sets', 'Repeticiones']], index=False)
+unique_values = st.session_state['Progreso_ind'].drop_duplicates(subset=['Dia', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])
+st.write(unique_values[['Dia', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Sets', 'Repeticiones']], index=False)
 
-# Gráfico de comparación entre personas
-st.subheader("📈 Comparación de progreso entre personas")
-avg_peso = st.session_state['Progreso_ind'].groupby('Persona')['Peso'].mean().reset_index()
-fig_avg_peso = px.bar(avg_peso, x='Persona', y='Peso', title='Promedio de peso levantado por persona', color='Persona', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
-st.plotly_chart(fig_avg_peso)
+# Gráficos adicionales
+st.subheader("📊 Comparación de progreso entre personas")
+if 'Progreso_ind' in st.session_state:
+    avg_peso = st.session_state['Progreso_ind'].groupby('Persona')['Peso'].mean().reset_index()
+    fig_avg_peso = px.bar(avg_peso, x='Persona', y='Peso', title='Promedio de peso levantado por persona', color='Persona', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
+    st.plotly_chart(fig_avg_peso)
 
 # Histograma de repeticiones por máquina y persona
 st.subheader("📊 Histograma de repeticiones por máquina y persona")
-fig_hist_rep = px.histogram(st.session_state['Progreso_ind'], x='Repeticiones', color='Persona', title='Distribución de repeticiones por máquina y persona', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
-st.plotly_chart(fig_hist_rep)
+if 'Progreso_ind' in st.session_state:
+    fig_hist_rep = px.histogram(st.session_state['Progreso_ind'], x='Repeticiones', color='Persona', title='Distribución de repeticiones por máquina y persona', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
+    st.plotly_chart(fig_hist_rep)
 
 # Box plot de pesos por día y persona
 st.subheader("📊 Box plot de pesos por día y persona")
-fig_box_peso = px.box(st.session_state['Progreso_ind'], x='Día', y='Peso', color='Persona', title='Distribución de pesos por día y persona', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
-st.plotly_chart(fig_box_peso)
+if 'Progreso_ind' in st.session_state:
+    fig_box_peso = px.box(st.session_state['Progreso_ind'], x='Dia', y='Peso', color='Persona', title='Distribución de pesos por día y persona', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
+    st.plotly_chart(fig_box_peso)
 
 # Gráfico de línea de series por día
-st.subheader("📈 Gráfico de línea de series por día")
-fig_line_sets = px.line(st.session_state['Progreso_ind'], x='Día', y='Sets', color='Persona', markers=True, title='Número de series por día', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
-st.plotly_chart(fig_line_sets)
+st.subheader("📊 Gráfico de línea de series por día")
+if 'Progreso_ind' in st.session_state:
+    fig_line_sets = px.line(st.session_state['Progreso_ind'], x='Dia', y='Sets', color='Persona', markers=True, title='Número de series por día', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
+    st.plotly_chart(fig_line_sets)
 
 # Diagrama de dispersión de peso vs repeticiones
 st.subheader("📊 Diagrama de dispersión de peso vs repeticiones")
-fig_scatter_peso_rep = px.scatter(st.session_state['Progreso_ind'], x='Peso', y='Repeticiones', color='Persona', title='Peso vs Repeticiones', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
-st.plotly_chart(fig_scatter_peso_rep)
+if 'Progreso_ind' in st.session_state:
+    fig_scatter_peso_rep = px.scatter(st.session_state['Progreso_ind'], x='Peso', y='Repeticiones', color='Persona', title='Peso vs Repeticiones', color_discrete_map={'Carlos': 'black', 'Cinthia': 'skyblue'})
+    st.plotly_chart(fig_scatter_peso_rep)
