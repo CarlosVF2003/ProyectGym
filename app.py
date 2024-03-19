1e46cb5f0e2f76d9734f0cd0bff4aaa588aea764
Carlos Alexander Vera Figueroa <162856389+CarlosVF2003@users.noreply.github.com>
2024-03-19 09:34:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ with st.expander('📝 Registro de Datos'):
             st.session_state['Progreso_ind'] = pd.concat([st.session_state['Progreso_ind'], Progreso_new], ignore_index=True)
             # Guardar el DataFrame actualizado en un archivo CSV
             # Utiliza transform para agregar la columna de conteo directamente al DataFrame existente
-            if Enfoque == 'Hipertrofia Muscular':
+            if Enfoque != 'Hipertrofia Muscular':
                 st.session_state['Progreso_ind']['Sets'] = st.session_state['Progreso_ind'].groupby(['Dia', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])[['Peso', 'Repeticiones']].transform('size')
         st.session_state['show_enfoque_form'] = False 
         st.success('¡Datos registrados con éxito!')
