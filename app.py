7ade907f6e8e4d0130032deae466ecfed918b010
Carlos Alexander Vera Figueroa <162856389+CarlosVF2003@users.noreply.github.com>
2024-03-25 07:23:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,12 @@ st.session_state['Progreso_ind'] = st.session_state['Progreso_ind'][
     (st.session_state['Progreso_ind']['Maquina'].isin(maquina_filtro))
 ]
 
+with st.expander('📓 Datos Registrados'):
+    st.subheader("Visualización de datos registrados")
+    # Eliminar filas duplicadas basadas en las columnas específicas y actualizar los sets
+    unique_values = st.session_state['Progreso_ind'].drop_duplicates(subset=['Dia', 'Persona', 'Maquina', 'Peso', 'Descanso', 'Repeticiones'])
+    st.dataframe(unique_values.reset_index(drop=True))
+    st.markdown(download_csv(unique_values, 'Progreso'), unsafe_allow_html=True)
          
 # Mostrar tablas de datos de Carlos y Cinthia
 with st.expander('🤵‍♂️ Tabla de datos de Carlos'):
