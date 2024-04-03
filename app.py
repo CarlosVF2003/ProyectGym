36d45d67e75fd6593dfe010be8cac4429febc30b
Carlos Alexander Vera Figueroa <162856389+CarlosVF2003@users.noreply.github.com>
2024-04-03 17:24:57 +0000

    commit

--- app.py
+++ app.py
@@ -188,19 +188,23 @@ if 'Progreso_ind' in st.session_state:
     with tab1:
         st.header("Cuadriceps (A)")
         df_cuadriceps = df[df['GM'] == 'A']
+        df_cuadriceps = df_cuadriceps.reset_index(drop=True)  # Resetear el índice para evitar problemas con Altair
         crear_graficos(df_cuadriceps, colores)
-    
+
     with tab2:
         st.header("Espalda y Biceps (B)")
         df_espalda_biceps = df[df['GM'] == 'B']
+        df_espalda_biceps = df_espalda_biceps.reset_index(drop=True)  # Resetear el índice para evitar problemas con Altair
         crear_graficos(df_espalda_biceps, colores)
-    
+
     with tab3:
         st.header("Gluteos y femorales (C)")
         df_gluteos_femorales = df[df['GM'] == 'C']
+        df_gluteos_femorales = df_gluteos_femorales.reset_index(drop=True)  # Resetear el índice para evitar problemas con Altair
         crear_graficos(df_gluteos_femorales, colores)
-    
+
     with tab4:
         st.header("Hombro, tricep y pecho (D)")
         df_pectoral_hombros_triceps = df[df['GM'] == 'D']
+        df_pectoral_hombros_triceps = df_pectoral_hombros_triceps.reset_index(drop=True)  # Resetear el índice para evitar problemas con Altair
         crear_graficos(df_pectoral_hombros_triceps, colores)
