297a95dba07330fe29941228bd0bdfe6196526fa
Carlos Alexander Vera Figueroa <162856389+CarlosVF2003@users.noreply.github.com>
2024-04-03 15:42:44 +0000

    committed

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ with st.expander('📓 Datos Registrados'):
     st.subheader("Visualización de datos registrados")
     # Eliminar filas duplicadas basadas en las columnas específicas y actualizar los Sets
     unique_values = st.session_state['Progreso_ind'].drop_duplicates(subset=['Dia', 'Persona', 'Maquina', 'Peso', 'Sets', 'Repeticiones', 'Descanso'])
-    st.dataframe(unique_values.reSets_index(drop=True))
+    st.dataframe(unique_values.reset_index(drop=True))
     st.markdown(download_csv(unique_values, 'Progreso'), unsafe_allow_html=True)
     df_filtred = unique_values
     
