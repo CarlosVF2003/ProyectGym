ba8b372d781cf2d0220c60b60a28e69d359fbb3a
Carlos Alexander Vera Figueroa <162856389+CarlosVF2003@users.noreply.github.com>
2024-04-09 00:24:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -172,10 +172,9 @@ with st.expander('📓 Datos Registrados'):
     # Eliminar filas duplicadas basadas en las columnas específicas y actualizar los sets
     unique_values = gym_original.drop_duplicates(subset=['Dia', 'Persona', 'Maquina','Peso','Sets', 'Repeticiones','Descanso'])
     st.dataframe(unique_values.reset_index(drop=True))
+    st.markdown(download_csv(unique_values, "datos_registrados"), unsafe_allow_html=True)
     df = unique_values
     # Botón para descargar la tabla de datos
-    if st.button("Descargar tabla de datos como CSV"):
-        st.markdown(download_csv(unique_values, "datos_registrados"), unsafe_allow_html=True)
 
 # %%
 # Mostrar tablas de datos de Carlos y Cinthia
